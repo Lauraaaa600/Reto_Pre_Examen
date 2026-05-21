41b2f4c18dc26c4d28888dba3fe5f61dd379df6e
Laura Herrera <est.lauraa.herrerae@unimilitar.edu.co>
2026-05-21 10:51:00 -0500

    se comenzo a ahcer el constructor q recibe el numero de soldados

---
+++ src/retopreexamen/RetoPreExamen.java
@@ -0,0 +1,46 @@
+
+package retopreexamen;
+import java.util.Scanner;
+
+class Lectura {
+  private static Scanner inp = new Scanner(System.in);
+  public static int nextInt(String mss) {
+    System.out.print(mss);
+    return inp.nextInt();
+  }
+}
+
+class FilaIndia{
+    private int[]soldados;
+    public FilaIndia (int n, Lectura lectura){
+        soldados=new int[];
+        for (int i = 0; i<n;i++){
+            System.out.print("Siguiente soldado: ");
+        }
+        
+    }
+    
+
+
+
+}
+
+public class SupervisionSoldados {
+	public static void main(String[] args) {
+		FilaIndia filaI = new FilaIndia();
+		int comandantes[], subordinados[];
+
+		int n = Lectura.nextInt("Cuántos soldados en la fila: ");
+		for (int i = 0; i < n; i++)
+		  filaI.add(Lectura.nextInt("Siguiente soldado: "));
+
+		int s = Lectura.nextInt("Cuántos supervisados: ");
+		comandantes = new int[s];
+		subordinados = new int[s];
+		for (int i = 0; i < s; i++) {
+		  comandantes[i] = Lectura.nextInt("Siguiente soldado supervisor: ");
+		  subordinados[i] = Lectura.nextInt("Siguiente soldado sobordinado: ");
+		}
+		filaI.revisar(comandantes, subordinados);
+	}
+}
